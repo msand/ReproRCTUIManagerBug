c309ee3d749a43c910eb1c5f7c2d3e10de283aae
Mikael Sand <msand@abo.fi>
2019-02-09 01:39:00 +0200

    Code example to reproduce RCTUIManager bug report

--- App.js
+++ App.js
@@ -1,50 +1,136 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Dimensions,
+  StyleSheet,
+} from 'react-native';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+const { height } = Dimensions.get('window');
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {
+  VictoryChart,
+  VictoryBar,
+  VictoryGroup,
+  VictoryZoomContainer,
+  VictoryClipContainer,
+} from 'victory-native';
 
-type Props = {};
-export default class App extends Component<Props> {
+class FixedVictoryClipContainer extends VictoryClipContainer {
+  renderClippedGroup(props, clipId) {
+    const {
+      style,
+      events,
+      transform,
+      children,
+      className,
+      groupComponent,
+    } = props;
+    const clipComponent = this.renderClipComponent(props, clipId);
+    const groupProps = {
+      className,
+      style,
+      transform,
+      key: `clipped-group-${clipId}`,
+      clipPath: `url(#${clipId})`,
+      events,
+    };
+    return React.cloneElement(groupComponent, groupProps, [
+      ...React.Children.toArray(children),
+      clipComponent,
+    ]);
+  }
+}
+
+class BrokenVictoryClipContainer extends VictoryClipContainer {
+  renderClippedGroup(props, clipId) {
+    const {
+      style,
+      events,
+      transform,
+      children,
+      className,
+      groupComponent,
+    } = props;
+    const clipComponent = this.renderClipComponent(props, clipId);
+    const groupProps = {
+      className,
+      style,
+      transform,
+      key: `clipped-group-${clipId}`,
+      clipPath: `url(#${clipId})`,
+      events,
+    };
+    return React.cloneElement(groupComponent, groupProps, [
+      clipComponent,
+      ...React.Children.toArray(children),
+    ]);
+  }
+}
+
+export default class Chart extends Component {
+  state = { length: 3 };
+  inc = () => {
+    this.setState(({ length }) => {
+      return { length: length + 1 };
+    });
+  };
+  dec = () => {
+    this.setState(({ length }) => {
+      return { length: length - 1 };
+    });
+  };
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View style={style.container}>
+        <View style={style.buttons}>
+          <TouchableOpacity onPress={this.inc} style={style.button}>
+            <Text>Increment</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this.dec} style={style.button}>
+            <Text>Decrement</Text>
+          </TouchableOpacity>
+        </View>
+        <VictoryChart
+          height={height - 100}
+          containerComponent={
+            <VictoryZoomContainer
+              clipContainerComponent={<BrokenVictoryClipContainer />}
+            />
+          }
+        >
+          <VictoryGroup
+            animate={{ duration: 1000, onLoad: { duration: 1000 } }}
+          >
+            {Array.from(this.state).map((_, i) => (
+              <VictoryBar
+                key={i}
+                data={[
+                  {
+                    x: 1 + i * 0.5,
+                    y: 1 + i * 10,
+                  },
+                  {
+                    x: 2 + i * 0.5,
+                    y: 2 + i * 10,
+                  },
+                ]}
+              />
+            ))}
+          </VictoryGroup>
+        </VictoryChart>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
+const style = StyleSheet.create({
+  container: { backgroundColor: 'white', paddingTop: 40 },
+  buttons: { flexDirection: 'row' },
+  button: {
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'black',
   },
 });
